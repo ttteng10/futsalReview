6cf522e0a49bacd6a12057412853b9a9416ba0db
ttteng10 <110088222+ttteng10@users.noreply.github.com>
2025-07-30 17:19:56 +0900

    vercel analytics add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import store from "./redux/index.js";
 import "./index.css";
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>
 );
